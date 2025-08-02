e4c3bf594e987bf3c84ea938c5ad6e7ac566eb0b
Alex <welmozhko88@gmail.com>
2025-08-02 22:27:24 +0300

    added pagination btns

--- components/task-area/pagination/pagination-area.tsx
+++ components/task-area/pagination/pagination-area.tsx
@@ -1,4 +1,9 @@
-// import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
+
+import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+import { BiFirstPage, BiLastPage } from 'react-icons/bi';
+
+import { PaginationSelection } from './pagination-selection';
 
 export const PaginationArea = () => {
   return (
@@ -6,12 +11,56 @@ export const PaginationArea = () => {
       <span className='text-slate-600 text-sm'>0 of 36 row(s) selected.</span>
 
       <div className='flex items-center gap-14'>
-        {/* PAG SELECTION */}
+        <PaginationSelection />
 
         <div className='flex gap-6 items-center'>
           <span className='text-sm font-medium'>Page 1 of 4 </span>
 
-          <div></div>
+          <div className='flex items-center justify-end space-x-2'>
+            {/*____________FIRST PAGE BTN _________________________________________*/}
+            <Button
+              variant='outline'
+              size='sm'
+              className='size-9 w-12'
+              // onClick={() => table.setPageIndex(0)}
+              // disabled={!table.getCanPreviousPage()}
+            >
+              <BiFirstPage />
+            </Button>
+
+            {/*____________PREVIOUS PAGE BTN ______________________________________*/}
+            <Button
+              variant='outline'
+              size='sm'
+              className='size-9 w-12'
+              // onClick={() => table.prevPage()}
+              // disabled={!table.getCanPreviousPage()}
+            >
+              <GrFormPrevious />
+            </Button>
+
+            {/*____________NEXT PAGE BTN __________________________________________*/}
+            <Button
+              variant='outline'
+              size='sm'
+              className='size-9 w-12'
+              // onClick={() => table.nextPage()}
+              // disabled={!table.getCanNextPage()}
+            >
+              <GrFormNext />
+            </Button>
+
+            {/*____________LAST PAGE BTN __________________________________________*/}
+            <Button
+              variant='outline'
+              size='sm'
+              className='size-9 w-12'
+              // onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              // disabled={!table.getCanNextPage()}
+            >
+              <BiLastPage />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/task-area/pagination/pagination-selection.tsx
@@ -0,0 +1,3 @@
+export const PaginationSelection = () => {
+  return <div>Pagination Selection</div>;
+};
